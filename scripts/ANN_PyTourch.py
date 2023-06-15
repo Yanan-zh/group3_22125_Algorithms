452ec4154dd09adbd3ff53c0ed9156ccb5dd23b7
Einarr5 <136359563+Einarr5@users.noreply.github.com>
2023-06-15 14:42:54 +0200

    Update ANN_PyTourch.py

--- scripts/ANN_PyTourch.py
+++ scripts/ANN_PyTourch.py
@@ -3,7 +3,6 @@ import torch
 import math
 from torch.autograd import Variable
 import torch.nn as nn
-#import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 
